da8a70539d5f1227e605fa86c2e496df7618bc25
Luc626 <lucasst626@gmail.com>
2023-11-28 14:06:23 +0100

    cosas random added

--- Ejercicion_1/ejercicio_1.js
+++ Ejercicion_1/ejercicio_1.js
@@ -1,3 +1,11 @@
-function showText(){
+function createText(){
+    let contenedor = document.createElement("p")
+    contenedor = document.appendChild("p");
+    return;
+}
+
+function generateText(texto){
+    const contenedor = document.querySelector("#contenedor")
+    contenedor.innerHTML = 
     
 }
